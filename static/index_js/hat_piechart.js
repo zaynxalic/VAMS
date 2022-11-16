da3c9c3e6a3017efdae100baa0ca3223c3d7e28f
zaynxalic <30385887+zaynxalic@users.noreply.github.com>
2022-11-16 16:56:20 +1100

    Update hat_piechart.js

--- static/index_js/hat_piechart.js
+++ static/index_js/hat_piechart.js
@@ -1,4 +1,4 @@
-// mychart初始化
+// For initialisation
 var hatchart = echarts.init(document.getElementById('wearing_hat'));
 
 var eventNum_2 = 0;
@@ -6,9 +6,9 @@ var eventNum_2 = 0;
 var hatArea;
 
 var hat_data;
-
+// When no data is shown
 const no_data = '暂无数据';
-// 如果无法正常获取数据
+
 var succeedGainHatData = true;
 
 const noDataColor = ['grey']
@@ -19,39 +19,25 @@ var hat_data_color;
 
 var hat_labels = [''];
 function setajax_hat(){
-// 默认数据
   hat_data = [{'name':no_data,'value':0}];
   hat_labels = [''];
-
-  // // 显示加载动画,无数据
-  // hatchart.showLoading({
-  //   text: '加载中',
-  //   color: '#ffffff',
-  //   textColor: '#8a8e91',
-  //   maskColor: 'rgba(255, 255, 255, 0.8)',
-  // });
 
   $.ajax({
     method:"GET",
     url:"http://172.16.138.39/quickstart/camera-list/",
-    // 当成功的时候 获取数据
     success:function(h){
 
       succeedGainHatData = true;
-      // 获取事件个数
       eventNum_2 = h.count;
 
       for (var i = 0;i <eventNum_2;i++){
-        // 查看是否为闯入事件
         hatArea = (JSON.parse(h.results)[i]).fields.camera_from;
-        // alert(hatArea);
          if ((JSON.parse(h.results)[i]).fields.eventName == "noHelmet"){
-          // 请求获取摄像机位置
+          // Request for camera position
 
-           // 获取区域事件个数
+           // Get the number of events in the area
           var oneEventCount_2 = parseInt((JSON.parse(h.results)[i]).fields.count);
          
-          // 如果list 中无值
           if (hat_data[0].name == no_data){
 
             hat_data[0].name = hatArea;
@@ -62,14 +48,14 @@ function setajax_hat(){
 
             var flag = true;
             for (var j=0;j<hat_data.length;j++){
-              // 如果有该区域
+              // if the area is obtianed
               if(hat_data[j].name == hatArea){
                 flag = false;
                 hat_data[j].value = hat_data[j].value + oneEventCount_2;
               }
             }
 
-            // 如果无该区域
+            // If the area does not exist
             if (flag){
               hat_data.push({'name':hatArea,'value':oneEventCount_2})
               hat_labels.push(hatArea);
@@ -84,7 +70,7 @@ function setajax_hat(){
 
         if ((hat_data[0].name !== '暂无数据')){
 
-          // 隐藏loading 页面
+          // hide the loading screen
           // hatchart.hideLoading();
           hat_data_color = DataColor;
           // setchart
@@ -98,7 +84,7 @@ function setajax_hat(){
       
 
     },
-    // 当失败的时候 输出错误
+    // When it fails Output error
     error: function(error_data){
       succeedGainHatData = false;
       console.log("error");
@@ -109,12 +95,11 @@ function setajax_hat(){
 
 
 setajax_hat();
-// 每隔一段时间获取信息
+// Access to information at regular intervals
 setInterval(function(){
   setajax_hat();
 },60000)
 
-// 设置帽子方程
 function sethatChart(){
   var option = {
 
@@ -123,15 +108,14 @@ function sethatChart(){
   
       x: 'left',
       y: 'top',
-      // legend 的位置
   
-      itemWidth: 24,   // 设置图例图形的宽
-      itemHeight: 18,  // 设置图例图形的高
+      itemWidth: 24,   
+      itemHeight: 18, 
       textStyle: {
-        color: '#666'  // 图例文字颜色
+        color: '#666' 
       },
-      // itemGap设置各个item之间的间隔，单位px，默认为10，横向布局时为水平间隔，纵向布局时为纵向间隔
-      backgroundColor: '#eee',  // 设置整个图例区域背景颜色
+   
+      backgroundColor: '#eee',  
       data: hat_labels,
   
     },
@@ -140,32 +124,25 @@ function sethatChart(){
         x: 'center',
         y: 'center',
         type: 'pie',
-        radius: ['35%', '60%'],  // 设置环形饼状图， 第一个百分数设置内圈大小，第二个百分数设置外圈大小
-        data: hat_data, //默认数据
+        radius: ['35%', '60%'], 
+        data: hat_data, 
         avoidLabelOverlap: false,
-        // itemStyle 设置饼状图扇形区域样式
         itemStyle: {
-          // emphasis：英文意思是 强调;着重;（轮廓、图形等的）鲜明;突出，重读
-          // emphasis：设置鼠标放到哪一块扇形上面的时候，扇形样式、阴影
           emphasis: {
             shadowBlur: 10,
             shadowOffsetX: 0,
             shadowColor: 'rgba(30, 144, 255，0.5)'
           }
         },
-        // 设置值域的那指向线
         labelLine: {
           normal: {
-            show: true   // show设置线是否显示，默认为true，可选值：true ¦ false
+            show: true   
           }
         },
-        // 设置值域的标签
+  
         label: {
           normal: {
-            position: 'outer',  // 设置标签位置，默认在饼状图外 可选值：'outer' ¦ 'inner（饼状图上）'
-            // formatter: '{a} {b} : {c}个 ({d}%)'   设置标签显示内容 ，默认显示{b}
-            // {a}指series.name  {b}指series.data的name
-            // {c}指series.data的value  {d}%指这一部分占总数的百分比
+            position: 'outer', 
             formatter: '{b}\n{c}'
           }
         },
@@ -189,17 +166,17 @@ function sethatChart(){
       },
     ],
     grid: {
-      left: 0,   //距离左边的距离
-      right: 0, //距离右边的距离
-      bottom: 0,//距离下边的距离
-      top: 0 //距离上边的距离
+      left: 0,   
+      right: 0, 
+      bottom: 0,
+      top: 0
     },
 
     color:hat_data_color,
 
   };
   
-  hatchart.setOption(option);//*****自己要定义，定时器只是为了动态改变数据
+  hatchart.setOption(option);
 
 }
 
